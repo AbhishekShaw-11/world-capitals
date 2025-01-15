b81dfe55594ae5efe9df9bd083f0c639380e1ebb
abhishek 1199 <abhishekshaw1199@gmail.com>
2025-01-15 09:05:44 +0530

    contact

--- world-capital/src/pages/Contact.jsx
+++ world-capital/src/pages/Contact.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
+const handleFormSubmit=(formData)=>{
+  const formInputData = Object.fromEntries(formData.entries());
+  //console.log(formInputData)
+}
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
-  )
-}
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            required
+            placeholder="Enter your name"
+            name="username"
+            autoComplete="false"
+          />
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter your email address"
+            name="email"
+            required
+            autoComplete="false"
+          />
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            name="message"
+            autoComplete="false"
+          ></textarea>
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
