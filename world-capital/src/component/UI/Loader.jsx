0a78649b0253d4e6b8be5d6f84ac8f3005439155
abhishek 1199 <abhishekshaw1199@gmail.com>
2025-01-17 11:52:40 +0530

    data

---
+++ world-capital/src/component/UI/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+/* HTML: <div class="loader"></div> */
+import "../../App.css"
+const Loader = () => {
+  return (
+  <div className="loader">
+
+    </div>
+  )
+}
+
+export default Loader
--- world-capital/src/pages/Country.jsx
+++ world-capital/src/pages/Country.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React, { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import Loader from "../component/UI/Loader";
+import CountryCard from "../component/Layout/CountryCard";
 const Country = () => {
-  return (
-    <div>
-      <h1>Country</h1>
-    </div>
-  )
-}
+  const [isPending, startTransition] = useTransition();
+  const [countries, setContries] = useState([]);
 
-export default Country
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setContries(res.data);
+    });
+  }, []);
+
+  if (isPending)
+    return (
+      <h1>
+        <Loader />
+      </h1>
+    );
+  return (
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {countries.map((curCountry) => {
+          return <CountryCard country={curCountry} />;
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Country;
