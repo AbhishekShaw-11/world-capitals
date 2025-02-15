c5eaf963ae6d455ca31c57844b98ce9c6fcf040e
abhishek 1199 <abhishekshaw1199@gmail.com>
2025-02-15 17:42:11 +0530

    basci context api

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './Component/Login';
+import Profile from './Component/Profile';
+import UserContextProvider from './Context/UserContextProvider';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hii</h1>
-    </div>
+    <UserContextProvider>
+      <h1>Hii</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   );
 }
 
